e377f1054b66330320a79108b50cd23ea6670a3d
jing520 <78027883+ljin520@users.noreply.github.com>
2022-12-12 20:10:32 -0800

    hidingFirbaseInformation

--- Firebase/firebase-setup.js
+++ Firebase/firebase-setup.js
@@ -5,12 +5,12 @@ import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAt_U-2vf40pQ4EcUnBapA_MkQ5LH4LLt8",
+  apiKey: "----------------------------------",
   authDomain: "amazingclothes-705ac.firebaseapp.com",
   projectId: "amazingclothes-705ac",
   storageBucket: "amazingclothes-705ac.appspot.com",
   messagingSenderId: "591583759383",
-  appId: "1:591583759383:web:169471b50cc39fb2d0e052",
+  appId: "------------------------------------",
   // measurementId: "G-965ZKNWTEV"
 };
 let myApp = initializeApp(firebaseConfig);
